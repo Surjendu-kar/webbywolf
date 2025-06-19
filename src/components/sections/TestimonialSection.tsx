94034493900792cad4b4a89fb1ab158884a99ca6
Surjendu kar <rahulgotp9988@gmai.com>
2025-06-19 13:35:47 +0530

    Refactor TestimonialSection for improved functionality and styling
    - Added smooth scrolling functionality for card selection using useRef.
    - Updated layout and styling for better responsiveness and visual appeal.
    - Enhanced card selection effects and adjusted dimensions for a more cohesive design.

--- src/components/sections/TestimonialSection.tsx
+++ src/components/sections/TestimonialSection.tsx
@@ -2,7 +2,7 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "../ui/button";
 
 interface Testimonial {
@@ -50,35 +50,53 @@ const TestimonialSection = () => {
   ];
 
   const [selectedCard, setSelectedCard] = useState(0);
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleCardSelect = (index: number) => {
+    setSelectedCard(index);
+    // Add small delay to allow for state update
+    setTimeout(() => {
+      const container = scrollContainerRef.current;
+      const selectedElement = container?.children[index];
+      if (selectedElement) {
+        selectedElement.scrollIntoView({
+          behavior: "smooth",
+          block: "nearest",
+          inline: "center",
+        });
+      }
+    }, 0);
+  };
 
   const handlePrevious = () => {
-    setSelectedCard((prev) =>
-      prev === 0 ? testimonials.length - 1 : prev - 1
-    );
+    const newIndex =
+      selectedCard === 0 ? testimonials.length - 1 : selectedCard - 1;
+    handleCardSelect(newIndex);
   };
 
   const handleNext = () => {
-    setSelectedCard((prev) =>
-      prev === testimonials.length - 1 ? 0 : prev + 1
-    );
+    const newIndex =
+      selectedCard === testimonials.length - 1 ? 0 : selectedCard + 1;
+    handleCardSelect(newIndex);
   };
 
   return (
     <section className="my-20 bg-brand-secondary">
-      <div className="container mx-auto flex flex-col gap-20 py-20">
-        <div className="flex items-center justify-between">
-          <div className="max-w-3xl text-white">
+      <div className="mx-auto flex flex-col gap-8 py-10 pl-4 lg:gap-16 lg:py-20 lg:pl-20">
+        {/* Title and Button */}
+        <div className="flex flex-col items-start justify-between gap-4 lg:flex-row lg:items-center lg:gap-0 ">
+          <div className="max-w-3xl space-y-2 text-white lg:space-y-4">
             <h2 className="text-2xl font-bold">Join other Sun harvesters</h2>
-            <h1 className="mt-4 font-roboto-condensed text-3xl font-bold  lg:text-5xl">
+            <h1 className="font-roboto-condensed text-3xl font-bold  lg:text-5xl">
               LOREM IPSUM DOLOR SIT AMET
             </h1>
-            <p className="mx-auto mt-6">
+            <p className="mx-auto">
               Dui euismod iaculis libero, aliquet vitae et elementum porttitor.
               Eleifend mi tristique condimentum congue fusce nunc, donec magnis
               commodo.
             </p>
           </div>
-          <div>
+          <div className="pr-0 lg:pr-20">
             <Button className="bg-white px-8 py-2 font-inter font-semibold text-brand-text transition-all duration-300 hover:-translate-y-0.5 hover:bg-brand-text hover:text-white hover:shadow-lg disabled:cursor-not-allowed disabled:opacity-50">
               Leorum Ipsum
               <ArrowRight className="ml-2 h-5 w-5" />
@@ -86,48 +104,58 @@ const TestimonialSection = () => {
           </div>
         </div>
 
-        <div className="flex items-center justify-center gap-6">
-          {testimonials.map((testimonial, index) => (
-            <Card
-              key={testimonial.id}
-              className={`flex h-[400px] w-[300px] cursor-pointer flex-col justify-between border-none bg-white transition-all duration-300 ${
-                index === selectedCard
-                  ? "scale-110 shadow-xl"
-                  : "scale-100 hover:scale-105"
-              }`}
-              onClick={() => setSelectedCard(index)}
-            >
-              <CardContent className="pt-2">
-                <div className="mb-4 flex justify-start">
-                  <Image
-                    src={testimonial.iconSrc}
-                    alt=""
-                    width={32}
-                    height={32}
-                    className="h-12 w-12"
-                  />
-                </div>
-                <p className="text-brand-text">{testimonial.content}</p>
-              </CardContent>
-              <CardFooter>
-                <div className="flex items-center space-x-3">
-                  <div className="relative h-12 w-12 overflow-hidden rounded-full">
-                    <Image
-                      src={testimonial.avatar}
-                      alt={testimonial.name}
-                      fill
-                      className="object-cover"
-                    />
-                  </div>
-                  <div>
-                    <h3 className="text-sm font-semibold text-gray-900">
-                      {testimonial.name}
-                    </h3>
-                  </div>
-                </div>
-              </CardFooter>
-            </Card>
-          ))}
+        <div className="relative flex w-full items-center justify-start gap-6  lg:justify-center">
+          <div
+            className="scrollbar-hide flex items-center gap-6 overflow-x-auto pl-5 pr-6"
+            ref={scrollContainerRef}
+          >
+            {testimonials.map((testimonial, index) => (
+              <div key={testimonial.id} className="py-8">
+                <Card
+                  className={`flex h-[350px] w-[300px] shrink-0 cursor-pointer flex-col justify-center border-none bg-white transition-all duration-300 lg:h-[400px] lg:w-[364px] ${
+                    index === selectedCard
+                      ? "z-10 scale-110 shadow-xl"
+                      : "scale-100 hover:scale-105"
+                  }`}
+                  onClick={() => handleCardSelect(index)}
+                >
+                  <CardContent className="">
+                    <div className="mb-4 flex justify-start">
+                      <Image
+                        src={testimonial.iconSrc}
+                        alt=""
+                        width={32}
+                        height={32}
+                        className="h-8 w-8 lg:h-12 lg:w-12"
+                      />
+                    </div>
+                  </CardContent>
+                  <CardContent>
+                    <p className="text-sm text-brand-text lg:text-base">
+                      {testimonial.content}
+                    </p>
+                  </CardContent>
+                  <CardFooter>
+                    <div className="flex items-center space-x-3">
+                      <div className="relative h-8 w-8 overflow-hidden rounded-full lg:h-12 lg:w-12">
+                        <Image
+                          src={testimonial.avatar}
+                          alt={testimonial.name}
+                          fill
+                          className="object-cover"
+                        />
+                      </div>
+                      <div>
+                        <h3 className="text-xs font-semibold text-gray-900 lg:text-sm">
+                          {testimonial.name}
+                        </h3>
+                      </div>
+                    </div>
+                  </CardFooter>
+                </Card>
+              </div>
+            ))}
+          </div>
         </div>
 
         {/* Navigation Buttons */}
