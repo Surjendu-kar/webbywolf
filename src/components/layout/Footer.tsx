b5428fea756f2936c8936eb99417b17da5b24a96
Surjendu kar <rahulgotp9988@gmai.com>
2025-06-19 16:46:35 +0530

    Update Footer background color for improved visual consistency

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="mt-10 bg-[#1B1F24] px-4 py-10 lg:mt-20 lg:px-0">
+    <footer className="mt-10 bg-[#171E2B] px-4 py-10 lg:mt-20 lg:px-0">
       <div className="container mx-auto flex flex-col gap-8 lg:gap-0">
         {/* Logo */}
         <div className="inline-flex max-w-24 justify-center bg-[#DBDBDB] px-4 py-2 md:max-w-28 md:px-6 lg:max-w-32 lg:px-8">
